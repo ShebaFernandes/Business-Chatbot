ddd40f137726c903ef78e95b6f4fac03fd49b8dd
ShebaFernandes <136631361+ShebaFernandes@users.noreply.github.com>
2025-09-24 11:46:23 +0530

    added requirements

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="business-chatbot",          # your project name
+    version="0.1.0",                  # version of your project
+    author="Sheba Paul",              # your name
+    author_email="shebafernandes5@gmail.com",  # your email
+    packages=find_packages(),         # automatically find packages in your project
+    install_requires=[]               # keep empty if using requirements.txt
+)
--- template.sh
+++ template.sh
@@ -10,4 +10,5 @@ touch .env
 touch app.py
 touch research/trials.ipynb
 touch requirements.txt
+touch setup.py
 
